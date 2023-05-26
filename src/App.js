11e4d10bae3d59afd29a3a1a2e07300b63b265df
andiarwidiar1 <andiarkurnia1234@gmail.com>
2023-05-26 21:05:05 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         transition={{ type: "spring", duration: 0.5 }}
         whileHover={{ scale: 1.1 }}
       >
-        Todo App
+        What's the plan for today?
       </motion.h1>
       <motion.div
         initial={{ y: 1000 }}
